fa63d58e2a5e050dfcf8b5d79db1aa0b0b108396
Leah Boyd <leahboyd08@gmail.com>
2019-03-25 18:59:09 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -50,7 +50,9 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-  holiday_hash[:winter].each do |
+  holiday_hash.each do | data |
+    data[:winter].each do | supplies |
+      
 end
 
 def all_supplies_in_holidays(holiday_hash)
