621b0d13ec203da932a15e3f2194429850ce0ef9
Leah Boyd <leahboyd08@gmail.com>
2019-03-25 18:46:22 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -39,7 +39,11 @@ end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
-  holiday_hash[season][holiday_name][supply_array]
+  holiday_hash = {
+      :fall => [
+        :known_for => "Ruby",
+        :languages => ["LISP", "C"]
+        }
   # remember to return the updated hash
   holiday_hash
 end
